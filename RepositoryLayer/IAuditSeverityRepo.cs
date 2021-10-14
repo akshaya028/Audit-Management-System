b9c2b9b41e9ba88ae610398f6cf55a178eea7c77
akshaya028 <56537937+akshaya028@users.noreply.github.com>
2021-10-14 22:43:19 +0530

    Add files via upload

---
+++ RepositoryLayer/IAuditSeverityRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuditSeverityMicroService.Models;
+
+namespace AuditSeverityMicroService.RepositoryLayer
+{
+    interface IAuditSeverityRepo
+    {
+        public void CreateAuditResponse(AuditRequest auditRequest, AuditResponse auditResponse, int projectId);
+        public int GetProjectCount(int projectId);
+        public AuditManagement ReadAuditManagement(int projectId);
+        public int ReadProjectId(string managerName);
+    }
+}
---
+++ ServiceLayer/IAuditSeverityService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuditSeverityMicroService.Models;
+using AuditSeverityMicroService.RepositoryLayer;
+
+namespace AuditSeverityMicroService.ServiceLayer
+{
+    interface IAuditSeverityService
+    {
+        public string GenerateAuditId();
+        public List<string> AuditResponseCalculation(int NoCount, string AuditType, List<AuditBenchmarkClass> benchMarkList);
+        public void CreateRepo(AuditRequest auditRequest, AuditResponse auditResponse, int projectId);
+        public int GetProjectIdCount(int projectId);
+        public AuditResponse ReadAuditResponse(int projectId);
+        public int GetProjectId(string managerName);
+
+    }
+}
