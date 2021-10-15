cadc8807e99d151ca8d8b52f9e15fcfeb006b978
akshaya028 <56537937+akshaya028@users.noreply.github.com>
2021-10-15 14:46:24 +0530

    Add files via upload

---
+++ Authorization Microservice/ServiceLayer/RepositoryWrapper.cs
@@ -0,0 +1,24 @@
+﻿using Authorization_Microservice.Models;
+using Authorization_Microservice.RepositoryLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization_Microservice.ServiceLayer
+{
+    public class RepositoryWrapper
+    {
+        private DataLayer dataLayer;
+        public RepositoryWrapper()
+        {
+            this.dataLayer = new DataLayer();
+        }
+        
+        public string Validate(PortalLoginDetails loginDetails)
+        {
+            string validationToken = this.dataLayer.Validate(loginDetails);
+            return validationToken;
+        }
+    }
+}
